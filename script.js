eca2e203a408e74e21010f2780b84e0eb06742b2
Nexusrex18 <lavisnj350@gmail.com>
2024-06-30 21:27:24 +0530

    ll

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
 function goTopage() {
     window.location.href = 'last.html';
 }
 
+const openModal = function () {
+  console.log('Button clicked')
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  setTimeout(goTopage, 1000)
+};
 
-document.querySelector('#signup').addEventListener('click', () => {
-    setTimeout(goTopage, 2000);
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 })
